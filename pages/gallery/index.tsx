917a9641a0322702ef61cbe2ece025f112177ec8
chirag-ghosh <cghosh828049@gmail.com>
2022-05-01 00:52:09 +0530

    Added gallery --continued;

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,8 +1,32 @@
 import type { NextPage } from 'next'
+import { getPhotoPaths } from '../../lib/gallery'
+
+export async function getStaticProps() {
+    const photoList = getPhotoPaths();
+    return {
+        props: {
+            photoList
+        }
+    }
+}
  
-const Gallery: NextPage = () => {
+const Gallery: NextPage = ({photoList}: any) => {
+    console.log(photoList.length)
     return(
-        <div>Gallery</div>
+        <div className='gallery'>
+            <h1>The beauties of the world</h1>
+            <div className='photo-grid'>
+                {photoList.map((photoPath: string, index: number) => {
+                    return(
+                        <img
+                            key={index}
+                            src={photoPath}
+                            alt={photoPath}
+                        />
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
