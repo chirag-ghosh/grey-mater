797d1dbde52956e6dab4b1057dd895c4449274ba
chirag-ghosh <cghosh828049@gmail.com>
2023-05-09 16:56:06 +0530

    Add vercel analytics;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import '../styles/app.scss'
 import "prismjs/themes/prism-okaidia.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
