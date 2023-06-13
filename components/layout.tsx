5daeeb9160ba364dcfdb50ffc8bb0e63dd6bd773
Chirag Ghosh <cghosh828049@gmail.com>
2023-06-13 12:26:21 +0530

    add metatags;

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,9 +9,22 @@ const Layout = ({children}: {children: any}) => {
     return(
         <div className="container">
             <Head>
-                <title>{biodata.name}</title>
+                <title>Chirag Ghosh</title>
+                <meta name="title" content="Chirag Ghosh" />
                 <meta name="description" content={biodata.tagline} />
                 <link rel="icon" href="/favicon.ico" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.chiragghosh.dev/" />
+                <meta property="og:title" content="Chirag Ghosh" />
+                <meta property="og:description" content={biodata.tagline} />
+                <meta property="og:image" content="/metatag.jpg" />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://www.chiragghosh.dev/" />
+                <meta property="twitter:title" content="Chirag Ghosh" />
+                <meta property="twitter:description" content={biodata.tagline} />
+                <meta property="twitter:image" content="/metatag.jpg" />
             </Head>
             <Header />
             {children}
