e7fc0c996069f5d13f27497ab996298f5324cfeb
Rajiv Harlalka <rajivharlalka009@gmail.com>
2025-04-20 02:23:12 +0530

    Update Desc

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -8,7 +8,7 @@ const About: NextPage = () => {
                     Chirag Ghosh
                 </div>
                 <div className="subtitle">
-                    A final year undergrad at IIT Kharagpur.
+                    An undergrad from IIT Kharagpur currently working at Amex.
                 </div>
                 <a href="mailto:cghosh828049@gmail.com">
                     Get in touch
@@ -19,8 +19,8 @@ const About: NextPage = () => {
             </div>
             <div className="text section">
                 <p>Hello There....</p>
-                <p>At the time of writing this, I am exploring the world of algorithms and web development.</p>
-                <p>I hate CP although I find DSA intriguing. I find the open source community awesome and starting to contribute in it. In my free time, I binge tech and cooking videos in youtube or watch a tv-series (mostly drama and comedy). I am a huge fan of friends and its the goto spot whenever I do not feel good.</p>
+                <p>At the time of writing this, I have moved past the world of algorithms and web development.</p>
+                <p>I hate CP (and will always do) although I find DSA intriguing. I find the open source community awesome and starting to contribute in it. In my free time, I binge tech and cooking videos in youtube or watch a tv-series (mostly drama and comedy). I am a huge fan of friends and its the goto spot whenever I do not feel good.</p>
                 <p>I love listening to music with choices changing bi-weekly. Though, I am more attached to those songs during JEE days than anything else. I still have that 7.1 GB folder of mixed English-Hindi songs which served me throughout those 2 years. Kinda the reason why Alan Walker is still my fav.</p>
                 <p>Oh! I love photography too. Mostly of nature and animals. I feel that whenever I go out to click photos, i move closer to nature, away from my regular table with laptop, keyboa...... you get the idea.</p>
                 <p>I love talking to people and listening to their experiences. In this blog, I plan to document my learnings and express other thoughts.</p>
